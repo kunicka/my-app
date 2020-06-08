e0eeb32390102e35192442cb3dbdfab2a342cbaf
Agnieszka <kuncika2000@gmail.com>
2020-06-08 12:09:50 +0200

    app base template

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import'./App.css';
+import {Players} from './components/Players';
+
+import './App.css';
+
+export const App:React.FC = () => {
+  return (
+    <div className="App">
+      <Players/>
+    </div>
+  );
+}
+
+
---
+++ src/components/Players.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export const Players:React.FC = () => {
+    return (
+        <div>
+            checking if app is working 
+        </div>
+    )
+}
