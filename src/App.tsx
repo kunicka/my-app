4409e273bd10dd6b3a065331e17c61f388fae146
Agnieszka <kuncika2000@gmail.com>
2020-06-08 14:41:13 +0200

    sum and average counting functionality added (map, reduce function)

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,21 @@ import playersArray from "./components/Players";
 
 export const App: React.FC = () => {
 
-  const playersArrayList = playersArray.map((player:any, idx:any) => (
+  const playersArrayList = playersArray.map((player:any, idx:number) => (
     <tr key={player.id}>
      <td >{player.username}</td> <td>{player.points}</td>
      </tr>
      
   ));
 
+
+  // const sumOfPoints = playersArrayList.reduce(function(a,b){return{points: a.points + b.points}})
+
+  const sumOfPoints:number = playersArray.reduce((previousScore, currentScore, index)=>previousScore+currentScore.points, 0);
+
+  const pointsAverage:number = playersArray.reduce((previousScore, currentScore, index)=>((previousScore+currentScore.points)/playersArray.length), 0);
+  
+  const shorterAverage = pointsAverage.toFixed(2);
   return (
     <div className="App">
       <table>
@@ -19,6 +27,12 @@ export const App: React.FC = () => {
           <th>Username</th> <th>Points</th>
         </tr>
         {playersArrayList}
+        <tr>
+        <th>Total</th> <th>{sumOfPoints}</th>
+        </tr>
+        <tr>
+        <th>Average</th> <th>{shorterAverage}</th>
+        </tr>
       </table>
     </div>
   );
