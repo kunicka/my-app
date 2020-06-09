2b7a8563e7edbe0a948630bcb4959abe57df6d7f
Agnieszka <kuncika2000@gmail.com>
2020-06-09 09:27:11 +0200

    hightlight color as a state, added to

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import playersArray from "./components/Players";
+
+export interface Player {
+  id:number,
+  username:string,
+  points:number,
+}
 
 
 export const App: React.FC = () => {
 
-  const playersArrayList = playersArray.map((player:any, idx:number) => (
+  const [highlightcolor, setHightlightcolor] = useState("red")
+  const playersArrayList = playersArray.map((player:any, index:number) => (
     <tr 
     key={player.id} 
-    style={{
-      backgroundColor:player.points>100?'red':'white',
-      color:player.points>100?'white':'black'}}
-      >
+    style ={{
+    backgroundColor:player.points>100?highlightcolor:'white',
+    color:player.points>100?'white':'black'}}  >
      <td>{player.username}</td> <td>{player.points}</td>
      </tr>
     ));
 
-  
-
+    const setStylePoints=(color:string):void => {
+      setHightlightcolor(color);
+    }
 
   const sumOfPoints:number = playersArray.reduce((previousScore, currentScore, index)=>previousScore+currentScore.points, 0);
 
@@ -26,6 +33,7 @@ export const App: React.FC = () => {
   const shorterAverage = pointsAverage.toFixed(2);
   return (
     <div className="App">
+      
       <table>
         <tr>
           <th>Username</th> <th>Points</th>
