b7e329045a63e9cdb8e717776d9a041132a08f22
Agnieszka <kuncika2000@gmail.com>
2020-06-10 13:44:11 +0200

    delete Player button is working

--- src/components/PlayersTable.tsx
+++ src/components/PlayersTable.tsx
@@ -1,8 +1,8 @@
-import React, {useState, PropsWithChildren} from "react";
+import React, { useState, PropsWithChildren } from "react";
 import playersArray from "./Players.json";
 
 export interface Color {
-  color: string 
+  color: string;
 }
 
 export interface Player {
@@ -17,33 +17,28 @@ export interface newPlayerProps {
   points: number;
 }
 
-export const PlayersTable:(
-    props: PropsWithChildren<Color>, 
-    children: any) 
-    => any = (props, children: any) => {
+export const PlayersTable: (
+  props: PropsWithChildren<Color>,
+  children: any
+) => any = (props, children: any) => {
+  const [playersList, setPlayersList] = useState<Array<Player>>(playersArray);
+  const [name, setName] = useState("");
 
-const [playersList, setPlayersList] = useState<Array<Player>>(playersArray);
-const [name, setName] = useState("");
-
-  let id = 0;
+  let id = 1;
 
   const addPlayer = () => {
     const points = Math.floor(Math.random() * 200) + 1;
-    id++;
-
     
-
     const Player = {
-        id:id,
-        username: name,
-        points: points
-    }
+      id: id,
+      username: name,
+      points: points,
+    };
+    setPlayersList([...playersList, Player]);
+    id++;
+};
 
-    
-
-      setPlayersList([...playersList, Player])
-
-      // const newArray = [
+    // const newArray = [
     //     ...playersList,
     //     Player
     //   ];
@@ -68,8 +63,7 @@ const [name, setName] = useState("");
     // druga opcja, ale też wyskakuje błąd:     setPlayersList([...playersList, Player]);
     //trzecia opcja concat:                     setPlayersList(playersList.concat(Player));
     // setPlayersList(playersList=>[...playersList, Player]);
-
-    };
+  
 
   const sumOfPoints: number = playersArray.reduce(
     (previousScore, currentScore, index) => previousScore + currentScore.points,
@@ -89,15 +83,15 @@ const [name, setName] = useState("");
         <input
           placeholder="Enter the name if the player"
           type="text"
-          onChange={(e)=>setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
         <button onClick={addPlayer}>add a new Player and see the score</button>
       </>
       <table>
-          <thead>
-        <tr>
-          <th>Username</th> <th>Points</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Username</th> <th>Points</th>
+          </tr>
         </thead>
         <tbody>
           {playersList.map((player) => (
@@ -110,10 +104,18 @@ const [name, setName] = useState("");
               }
             >
               <td>{player.username}</td> <td>{player.points}</td>
-              <td><button onClick={(e)=>{playersList.splice(player.id-1, 1)}}>Delete</button></td>
+              <td>
+                <button
+                  onClick={(e) => {
+                    setPlayersList((prevState) => ([...prevState.slice(0,player.id-1), ...prevState.slice(player.id)]
+                      ))
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
-          
         </tbody>
         <tfoot>
           <tr>
@@ -127,4 +129,3 @@ const [name, setName] = useState("");
     </div>
   );
 };
-
