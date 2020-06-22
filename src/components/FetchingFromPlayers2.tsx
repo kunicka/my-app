8908a4ca0492d3557e587bd3c4bb76f478307469
Agnieszka <kuncika2000@gmail.com>
2020-06-22 08:36:43 +0200

    Data Fetching Added

--- src/components/FetchingFromPlayers2.tsx
+++ src/components/FetchingFromPlayers2.tsx
@@ -1,28 +1,54 @@
-import React, {useEffect, useState} from 'react'
-import {Player} from "./PlayersTable"
+import React, {useState} from 'react'
+// import {Player} from "./PlayersTable"
 import {Color} from "./PlayersTable"
 
 export const Component:React.FC<Color> = ({highlightcolor}) => {
-    const [jsonPlayers, setJsonPlayers] = useState<Player[]>([])
-    const [loaded, setLoaded] = useState(false); //flagowanie, bo fetchowanie trwa
 
-  const fetchPlayers = async () => {
-    const data = await fetch ("./Players2.json");
-    const players = await data.json();
-    console.log(players);
-    setJsonPlayers(players);
-    console.log(jsonPlayers);
-    setLoaded(true);
+const [message, setMessage]=useState("message");
+
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+const value = e.target.value;
+console.log(value);
+fetch(`http://numbersapi.com/${value}/year?json`)
+.then(res => {
+  if (res.ok) {
+    return res
+  } throw Error ("błąd");
+})
+.then(res => res.json())
+.then(data => setMessage(data.text))
+.catch(err => console.log(err))
 }
 
-  useEffect (()=>{
-      fetchPlayers();
-  })
+
+
+//     const [jsonPlayers, setJsonPlayers] = useState<Player[]>([])
+//     const [loaded, setLoaded] = useState(false); //flagowanie, bo fetchowanie trwa
+
+//   const fetchPlayers = async () => {
+//     const data = await fetch ("./Players2.json");
+//     const players = await data.json();
+//     console.log(players);
+//     setJsonPlayers(players);
+//     console.log(jsonPlayers);
+//     setLoaded(true)
+// }
+
+//   useEffect (()=>{
+//       fetchPlayers();
+//   })
 
     return (
         <div>
-           
-        {loaded ? ( jsonPlayers.map((player) => (
+
+        <label id="message">Label</label>
+        <input type="text" name="message" onChange={handleChange}/>
+        <p>Odpowiedź: {message}</p>
+      
+
+
+        {/* {loaded ? ( jsonPlayers.map((player) => (
             <tr
               key={player.id}
               style={
@@ -42,9 +68,7 @@ export const Component:React.FC<Color> = ({highlightcolor}) => {
                 </button>
               </td>
             </tr>
-          ))): "Trwa ładowanie nowej listy graczy..."}
-        
-        
+          ))): "Trwa ładowanie nowej listy graczy..."} */}
         </div>
         )
 }
